a69f404252c0ae855cabdffeb56743277170d50a
Stenio Silveira <stenioas@gmail.com>
2025-08-02 13:58:26 -0300

    refactor: improvement description and styles

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -12,11 +12,12 @@ const ContactPage = () => {
             className="contactpage--description"
             aria-label="Texto informativo de contato"
           >
-            Tem dúvidas, sugestões ou precisa de ajuda? Estamos aqui para ouvir
-            você. Nossa equipe está pronta para oferecer o suporte que você
-            precisa e garantir a melhor experiência com o Severino App. Entre
-            em contato e envie suas dúvida, realize parcerias e outros
-            assuntos.
+            Tem dúvidas, sugestões ou precisa de ajuda?
+            <br />
+            Estamos aqui para ouvir você. Nossa equipe está pronta para
+            oferecer o suporte que você precisa e garantir a melhor experiência
+            com o Severino App. Entre em contato e envie suas dúvida, realize
+            parcerias e outros assuntos.
           </p>
           <ContactForm />
         </div>
